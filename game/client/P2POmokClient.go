1a79d2a777988f52c447c10c70850d963b07544b
Hayeong Jeong <hy78733@gmail.com>
2024-05-10 15:32:55 +0900

    fix: use go routine to read from server

--- game/client/P2POmokClient.go
+++ game/client/P2POmokClient.go
@@ -4,50 +4,63 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
-	"strconv"
 )
 
 type ServerMessage struct {
 	Nickname string
-	UdpAddr string
+	UdpAddr  string
 }
 
-func readIndexFromServer(conn net.Conn) (int, error) {
+func notifyToServer(conn net.Conn, nickname string) error {
+	// 서버에게 클라이언트의 닉네임과 UDP 주소를 전달한다.
+	message := ServerMessage{nickname, CONN_ADDR}
+	encodedMessage := encodeMessage(message)
+	_, err := conn.Write(encodedMessage)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func readIndexFromServer(ch chan int, eCh chan error, conn net.Conn) {
 	buffer := make([]byte, 16)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		log.Fatal(err)
-		return -1, err
-	}
-
-	index, err := strconv.Atoi(string(buffer[:n]))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return index, nil
-}
-
-func readMessageFromServer(conn net.Conn) (ServerMessage, error) {
-	buffer := make([]byte, 1024)
 	_, err := conn.Read(buffer)
 	if err != nil {
-		log.Fatal("failed to read", err)
-		return ServerMessage{}, err
+		eCh <- err
+		return
 	}
 
-	reader := bytes.NewBuffer(buffer)
-	message := new(ServerMessage)
+	index := int(buffer[0])
+	ch <- index
+}
 
-	err = gob.NewDecoder(reader).Decode(message)
-	if err != nil {
-		return ServerMessage{}, err
+func readMessageFromServer(ch chan ServerMessage, eCh chan error, conn net.Conn) {
+	for {
+		buffer := make([]byte, 1024)
+		_, err := conn.Read(buffer)
+		if err == io.EOF {
+			continue
+		}
+		if err != nil {
+			eCh <- err
+			return
+		}
+
+		reader := bytes.NewBuffer(buffer)
+		message := new(ServerMessage)
+
+		err = gob.NewDecoder(reader).Decode(message)
+		if err != nil {
+			eCh <- err
+			return
+		}
+
+		ch <- *message
 	}
-	log.Printf("%s joined from %s. UDP endpoint: %s\n", message.Nickname, conn.RemoteAddr().String(), message.UdpAddr)
-	return *message, nil
 }
 
 func encodeMessage(message ServerMessage) []byte {
@@ -62,45 +75,17 @@ func encodeMessage(message ServerMessage) []byte {
 const (
 	SERVER_CONN_TYPE = "tcp"
 	SERVER_CONN_ADDR = "localhost:5999"
-	CONN_ADDR = "localhost:12345"
+	CONN_ADDR        = "localhost:12345"
 )
-func connectServer(nickname string) (int, ServerMessage) {
+
+func connectServer(nickname string) net.Conn {
 	conn, err := net.Dial(SERVER_CONN_TYPE, SERVER_CONN_ADDR)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
 
 	fmt.Printf("Welcome to P2P Omok, %s! Server: %s\n", nickname, SERVER_CONN_ADDR)
-
-	// 서버에게 클라이언트의 닉네임과 UDP 주소를 전달한다.
-	message:= ServerMessage{nickname, CONN_ADDR}
-	encodedMessage := encodeMessage(message)
-	_, err = conn.Write(encodedMessage)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	index, err := readIndexFromServer(conn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if(index == 0) {
-		fmt.Println("Waiting for an opponent...")
-	}
-
-	opponent, err := readMessageFromServer(conn)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if index == 0 {
-		fmt.Printf("%s joined (%s). You play first.", opponent.Nickname, opponent.UdpAddr)
-	} else {
-		fmt.Printf("%s is waiting for you (%s).\n %s plays first.", opponent.Nickname, opponent.UdpAddr, opponent.Nickname)
-	}
-
-	return index, opponent
+	return conn
 }
 
 func main() {
@@ -109,8 +94,45 @@ func main() {
 	}
 	nickname := os.Args[1]
 
-	// index, opponent := 
-	connectServer(nickname)
+	// index, opponent :=
+	conn := connectServer(nickname)
+	defer conn.Close()
 
+	err := notifyToServer(conn, nickname)
+	if err != nil {
+		log.Fatal("notifyToServer: ", err)
+	}
+
+	var index int
+	var opponent ServerMessage
+
+	indexCh := make(chan int)
+	eCh := make(chan error)
+	go func() {
+		readIndexFromServer(indexCh, eCh, conn)
+	}()
+
+	msgCh := make(chan ServerMessage)
+
+	for {
+		select {
+		case index = <-indexCh:
+			if index == 0 {
+				fmt.Println("Waiting for an opponent...")
+			}
+
+			go func() {
+				readMessageFromServer(msgCh, eCh, conn)
+			}()
+
+		case opponent = <-msgCh:
+			if index == 0 {
+				fmt.Printf("%s joined (%s). You play first.", opponent.Nickname, opponent.UdpAddr)
+			} else {
+				fmt.Printf("%s is waiting for you (%s).\n %s plays first.", opponent.Nickname, opponent.UdpAddr, opponent.Nickname)
+			}
+		case err := <-eCh:
+			log.Fatal(err)
+		}
+	}
 }
-
